9ab668f8bc0856e5d9621399c5a9034234f2fa44
Inunganba <inunganbathok858@gmail.com>
2024-12-12 12:06:21 +0530

    Initial Commit

---
+++ Github/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Github
+//
+//  Created by Inunganba Thokchom on 12/12/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
